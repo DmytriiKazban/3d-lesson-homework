8735653729099e9d5df32770efeaad3417fa6132
dmytriikazban <dmytrii.kazban@gmail.com>
2023-11-24 23:45:40 +0100

    Homerowk is done

---
+++ src/1.js
@@ -0,0 +1,51 @@
+//Undefined
+let no
+console.log(no)
+
+//bigint
+let firstBigint = BigInt(32)
+let secondBigint = BigInt(321)
+let sumOfBigint = firstBigint + secondBigint
+console.log(sumOfBigint)
+
+//NaN
+console.log(isNaN("JWJW"))
+
+//Infinity
+let result = Infinity + 10
+console.log(result);
+
+// number
+let a = 2 + 2
+console.log(a)
+
+//String
+let b = "Hello" + "World"
+console.log(b.length)
+
+//symbol
+const symbol1 = Symbol('example');
+const symbol2 = Symbol('example');
+
+console.log(symbol1 === symbol2);
+
+//object
+let superObject = {
+    firstName: "Dmytrii",
+    lastName: "Kazban"
+}
+let key = "age"
+let howMuch = 100
+
+superObject[key] = howMuch
+console.log(superObject) 
+
+//Null
+let myVariable = null
+console.log(myVariable)
+
+
+//Boolean
+if (b = String){
+    return console.log(true)
+}
